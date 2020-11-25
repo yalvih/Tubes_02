f1e4880ae3b3a93c9e45366f49efaaa154067f48
Alfred <alfredapri2000@gmail.com>
2020-11-25 21:24:55 +0700

    Fragments' base code

--- app/src/main/java/com/example/tubes_02/view/GameOverFragment.java
+++ app/src/main/java/com/example/tubes_02/view/GameOverFragment.java
@@ -1,4 +1,48 @@
 package com.example.tubes_02.view;
 
-public class GameOverFragment {
+//Game over fragment
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.tubes_02.R;
+
+public class GameOverFragment extends Fragment implements View.OnClickListener {
+    private FragmentListener listener;
+
+    public static GameOverFragment newInstance(String title) {
+        GameOverFragment fragment = new GameOverFragment();
+        Bundle args = new Bundle();
+        args.putString("game_over", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.game_over_fragment, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentListener) {
+            this.listener = (FragmentListener)context;
+        }
+        else {
+            throw new ClassCastException(context.toString() + " must implement FragmentListener!");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+//        Onclick goes here
+    }
 }
--- app/src/main/java/com/example/tubes_02/view/PianoTilesGameFragment.java
+++ app/src/main/java/com/example/tubes_02/view/PianoTilesGameFragment.java
@@ -1,4 +1,48 @@
 package com.example.tubes_02.view;
 
-public class PianoTilesGameFragment {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.tubes_02.R;
+
+//Main game fragment
+
+public class PianoTilesGameFragment extends Fragment implements View.OnClickListener {
+    private FragmentListener listener;
+
+    public static PianoTilesGameFragment newInstance(String title) {
+        PianoTilesGameFragment fragment = new PianoTilesGameFragment();
+        Bundle args = new Bundle();
+        args.putString("main_game", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.piano_tiles_game_fragment, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentListener) {
+            this.listener = (FragmentListener)context;
+        }
+        else {
+            throw new ClassCastException(context.toString() + " must implement FragmentListener!");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+//        Onclick goes here
+    }
 }
